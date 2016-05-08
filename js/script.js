0136a027604bc862bf4fccf425631cde53b45971
olenakutsir <olena.kutsir@gmail.com>
2016-05-08 11:22:05 +0000

    d

--- js/script.js
+++ js/script.js
@@ -5,25 +5,6 @@ $(function() {
     $('.hidden').fadeIn(4000) 
 })
 
-$(function() {
-    $('#btn-send').click(function() {
-        $.ajax({
-            url: "https://formspree.io/olena.kutsir@gmail.com", 
-            method: "POST",
-            data: {
-                name: name.value,
-                email: email.value,
-                message: message.value},
-            dataType: "json"
-        })
-        .done(function () {
-            $('#form').html('<h1>Thank you!</h1>');
-        })
-        .fail(function (jqXHR, textStatus) {
-            $('#form').html('<h1>Error!</h1><br>'+textStatus);
-        });
-    })
-})
 
 
 function scrollToId(whereToScroll) {
@@ -43,3 +24,76 @@ $(function () {
         event.preventDefault();
     });
 })
+
+
+
+$(function() {
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "https://formspree.io/olena.kutsir@gmail.com",
+                type: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
