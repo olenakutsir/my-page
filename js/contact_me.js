6d2526bec2b755882767cd8f068f26af46d5c075
olenakutsir <olena.kutsir@gmail.com>
2016-05-08 12:17:04 +0000

    h

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/olena.kutsir@gmail.com/",
+                url: "https://formspree.io/olena.kutsir@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
